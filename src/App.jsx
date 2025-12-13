73ce5598b76bcf842a8aabbace5a472ebe1175f3
zjncoo <151642330+zjncoo@users.noreply.github.com>
2025-12-13 16:07:36 +0100

    risolto schermo bianco

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ const LissajousFigure = ({ gsr0, gsr1, compatibility }) => {
 
 const toRoman = (num) => {
   if (!num) return "";
-  const n = parseInt(num);
+  let n = parseInt(num);
   const lookup = { M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1 };
   let roman = '', i;
   for (i in lookup) {
